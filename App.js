83fc567a8473f7ef21c04f91bd884f7747a7e029
Talha <talhatahir1455@gmail.com>
2024-04-24 15:04:32 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./src/Redux/store";
+import CounterScreen from "./src/screens/CounterScreen";
+export default function App() {
+  return (
+    <Provider store={store} style={styles.container}>
+    <CounterScreen/>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
