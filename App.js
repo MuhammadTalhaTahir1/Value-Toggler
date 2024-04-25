ea97f63918898c22fcdd7479e863604c9ec89dd3
Talha <talhatahir1455@gmail.com>
2024-04-25 23:18:45 +0500

    Tw setup install 2.0

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./src/Redux/store";
 import CounterScreen from "./src/screens/CounterScreen";
+import tw from "twrnc";
 export default function App() {
   return (
     <Provider store={store} style={styles.container}>
-    <CounterScreen/>
+      <CounterScreen />
     </Provider>
   );
 }
